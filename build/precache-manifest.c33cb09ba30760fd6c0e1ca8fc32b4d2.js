c31e6191ee5ee0d19e2be4500ebaaf073673b87a
Gaius-1 <agbofrederick230@gmail.com>
2023-01-27 16:49:15 +0000

    build pages for ecommerce

---
+++ build/precache-manifest.c33cb09ba30760fd6c0e1ca8fc32b4d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ed14704de3e7048cb8d80c7e0cd8fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe1bdaf18844553aeb95",
+    "url": "/static/css/2.6f44476b.chunk.css"
+  },
+  {
+    "revision": "fe1bdaf18844553aeb95",
+    "url": "/static/js/2.6087e9b2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6087e9b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5758ac3faf8945fe8bb2",
+    "url": "/static/js/main.3ed09f19.chunk.js"
+  },
+  {
+    "revision": "71e0677ab2f28215227f",
+    "url": "/static/js/runtime-main.b234ba30.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
